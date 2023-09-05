dc6e373f7dd2f6f8d2fc0ff94e74ebdb2a7f03ac
JackieW0818 <JackieW0818@outlook.com>
2023-09-05 12:12:15 +1000

    jquery added

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-const StudentData = {
-    data:{
-        btn : document.querySelector("#but")
-    },
-
-    func:{
-        init:function(){
-            Index.func.getEnrolmentData();
-        },
-        getEnrolmentData:function(){
-            fetch("./controllers/studentEnrolment.php",{
-                method:'GET',
-                mode:'cors',
-                credentials:'include'
-            })
-            .then(response=> {
-                console.log(response);
-                return response.json()
-            })
-            .then(data=>{
-                console.log(data);
-            }).catch((error) =>{
-                console.log(error);
-                console.error("Error: ", error.message)
-            })
-        }
-    }
+function getEnrolmentData() {
+    fetch("./controllers/studentEnrolment.php", {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include'
+    })
+        .then(res => {
+            console.log(res);
+            return res.json()
+        })
+        .then(res => {
+            console.log(res);
+        }).catch((error) => {
+            console.log(error);
+            console.error("Error: ", error.message)
+        })
 }
