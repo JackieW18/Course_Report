a88d723fc36de3ab687d475f25b8bcc89f09e05f
JackieW0818 <JackieW0818@outlook.com>
2023-09-14 15:48:10 +1000

    Clear modal after page jump

--- index.js
+++ index.js
@@ -144,6 +144,7 @@ $(document).ready(function () {
             alert("Page out of range");
         } else {
             pageNumber = Number(input);
+            $("#pagination-input").val("");
             $("#jump-page-modal").modal('hide');
             getEnrolmentData();
         }
